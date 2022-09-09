16eb3eb3584835c30753c26e284f755f14bfec29
El-idn <amos.eden56@gmail.com>
2022-09-08 18:42:54 -0700

    4-puts.c_1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-*main - print the strings in the put function
+* main - print the strings in the put function
 *
-*Description: using the main function
+* Description: using the main function
 * this program prints "programming is like printing a multilingual puzzle
 * Return: 0
 */
