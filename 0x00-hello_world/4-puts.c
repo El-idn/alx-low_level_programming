7f2f6e0e7d184ad78bbb872ac9fae59ba4e41d6e
El-idn <amos.eden56@gmail.com>
2022-09-08 18:34:10 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - print the strings in the put function
+*
+*Description: using the main function
+* this program prints "programming is like printing a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"programming is like printing a multilingual puzzle");
+	return (0);
+}
