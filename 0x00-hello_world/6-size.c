48af903659db9bf760149a30a23c545804866a44
El-idn <amos.eden56@gmail.com>
2022-09-08 20:12:08 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,12 @@
 */
 int main(void)
 {
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
 	printf(Size of a char: 1 byte(s)\n, sizeof(c));
 	printf(Size of an int: 4 byte(s)\n, sizeof(i));
 	printf(Size of a long int: 8 byte(s)\n, sizeof(li));
